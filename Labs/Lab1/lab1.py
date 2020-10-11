0775e590948ee4963da0c376fff3415e4343b7f9
Anton Eriksson <antoeri@student.chalmers.se>
2020-10-11 12:18:11 +0100

    SSE

--- Labs/Lab1/lab1.py
+++ Labs/Lab1/lab1.py
@@ -28,3 +28,28 @@ def poly_vector(a, x_):
         y_[i] = poly(a, x_[i])
         
     return y_
+
+def SSE(Y, Y_):
+    if len(Y.shape)>1:
+        Y = np.squeeze(Y)
+
+    return np.linalg.norm(Y-Y_, ord=2)**2
+
+
+
+def compute_SSE(y,x,a):
+    y_hat=poly(a,x)
+    SSE=np.linalg.norm(y-y_hat, ord=2)**2
+    return SSE
+
+def indic(y,x,a):
+    SSE=compute_SSE(y,x,a)
+    print('\n')
+    print('B coeficients are:')
+    for i in range(0,len(a)):
+        print(a[i])
+    print('\n')
+    print('SSE is:')
+    print(SSE)
+    return
+    
