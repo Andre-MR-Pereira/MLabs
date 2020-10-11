a82aecfbf295a46f99b240ea843b8209a7cc7798
Anton Eriksson <antoeri@student.chalmers.se>
2020-10-11 10:48:28 +0100

    poly_fit

---
+++ Labs/Lab1/lab1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def poly_fit(x, y, p):
+    
+    X = np.zeros([len(x), p])
+
+    for row in range(0, X.shape[0]):
+        for col in range(0, X.shape[1]):
+            X[row, col] = x[row] ** col
+    
+    return np.matmul (np.matmul( np.linalg.inv( np.matmul(np.transpose(X), X) ), np.transpose(X) ), y)
